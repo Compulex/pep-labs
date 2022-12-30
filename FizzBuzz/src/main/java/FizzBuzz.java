1784abfe13adbe049763a1b489ce9a0e73662cf4
Alexa Lewis <alexalewis2013@afscholars.org>
2022-12-30 12:09:11 +0000

    Saved progress on FizzBuzz 2022-12-30 12:09:11 +0000

--- FizzBuzz/src/main/java/FizzBuzz.java
+++ FizzBuzz/src/main/java/FizzBuzz.java
@@ -15,6 +15,17 @@ public class FizzBuzz {
      * divisible by both, return 'FizzBuzz'. If none of these are true, return a blank string "".
      */
     public String FizzBuzzGen(int n){
-        return "";
+        String fizzbuzz = "";
+
+        if(n % 3 == 0){
+            fizzbuzz = "Fizz";
+        }
+        else if(n % 5 == 0){
+            fizzbuzz = "Buzz";
+        }
+        else if(n % 3 == 0 && n % 5 == 0){
+            fizzbuzz = "FizzBuzz";
+        }
+        return fizzbuzz;
     }
 }
