b2c69d25bf54129649d35294e7399977c094fe29
Alexa Lewis <alexalewis2013@afscholars.org>
2023-02-02 07:54:02 +0000

    Saved progress on ForeignKey 2023-02-02 07:54:02 +0000

--- ForeignKey/problem1.sql
+++ ForeignKey/problem1.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Post ( 
     id SERIAL PRIMARY KEY, 
     post varchar(255), 
-    user_fk int 
+    user_fk int /*REFERENCES User(id)*/
 );
 
-/*ALTER TABLE Post ADD FOREIGN KEY(user_fk) REFERENCES User(id);*/
+/*ALTER TABLE Post ADD FOREIGN KEY(user_fk) ;*/
