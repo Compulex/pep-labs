9c42fab06a1b925d4f4fb3f218084d28e99b895f
Alexa Lewis <alexalewis2013@afscholars.org>
2023-02-09 23:55:05 +0000

    Saved progress on ParentObjectClassMethods 2023-02-09 23:55:05 +0000

--- ParentObjectClassMethods/src/main/java/SampleClass.java
+++ ParentObjectClassMethods/src/main/java/SampleClass.java
@@ -56,9 +56,22 @@ public class SampleClass {
     public boolean b;
 
     //    implement a custom .equals(SampleClass other){} method here.
+    @Override
+    public boolean equals(SampleClass other){
+        if(other == this){
+            return true;
+        }
+        if(!(other instanceof SampleClass)){
+            return false;
+        }
 
+        boolean aCheck = (this.a == null && other.a == null) || (this.a != null && this.a.equals(other.a))
+
+    }
 
     //    implement a custom .toString(){} method here.
-
+    public String toString(){
+        return 
+    }
     
 }
