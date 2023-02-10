1e3e6ddbf46d392c7d980c6d9e193c697248ac01
Alexa Lewis <alexalewis2013@afscholars.org>
2023-02-10 06:08:33 +0000

    Saved progress on GroceryList 2023-02-10 06:08:33 +0000

--- ForeignKey/problem1.sql
+++ ForeignKey/problem1.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Post ( 
     id SERIAL PRIMARY KEY, 
     post varchar(255), 
-    user_fk int /*REFERENCES User(id)*/
+    user_fk int REFERENCES User(id)
 );
 
 /*ALTER TABLE Post ADD FOREIGN KEY(user_fk) ;*/
